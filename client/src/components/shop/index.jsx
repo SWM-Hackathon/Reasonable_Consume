c604bc8563dfdf41c67024618b2b7c0636d92b95
이중훈 <doublehoon99@gmail.com>
2021-05-13 16:59:47 +0000

    shop w mobile modified

--- client/src/components/shop/index.jsx
+++ client/src/components/shop/index.jsx
@@ -79,81 +79,96 @@ class Shop extends Component {
 	render() {
 		return (
 			<S.ShopContainer>
-				<div style={{ height: 'auto', width: '20vw', overflowY: 'auto' }}>
-					<select value={this.state.selectedFilter} onChange={this.filterChange} style={{width: '19vw', height: '5vh', textAlign: 'center', fontSize: '4vh'}}>
-						<option value="한식">한식</option>
-						<option value="중식">중식</option>
-						<option value="경양식,일식">경양식,일식</option>
-						<option value="기타외식업(다방,패스트푸드등)">
-							기타외식업(다방,패스트푸드등)
-						</option>
-					</select>
-					<S.ShopFilter>
-						{this.state.DATA.map((x) =>
-							x.sh_id === '00000000' ||
-							x.induty_code_se_name !== this.state.selectedFilter ? (
-								''
-							) : (
-								<button
-									type="button"
-									value={this.state.DATA.indexOf(x)}
-									style={{ height: '100px', fontSize: '3vh' }}
-									onClick={(e) =>
-										this.changeShopDetails(e, x.latitude, x.longitude)
-									}
-								>
-									{x.sh_name}
-								</button>
-							)
-						)}
-					</S.ShopFilter>
-				</div>
-				<S.ShopDetails>
-					<div><br />
-						<h1>{this.state.DATA[this.state.now].sh_name}</h1>
+				<S.ContainerPC>
+					<div style={{ height: 'auto', width: '20vw', overflowY: 'auto' }}>
+						<select
+							value={this.state.selectedFilter}
+							onChange={this.filterChange}
+							style={{
+								width: '19vw',
+								height: '5vh',
+								textAlign: 'center',
+								fontSize: '4vh',
+							}}
+						>
+							<option value="한식">한식</option>
+							<option value="중식">중식</option>
+							<option value="경양식,일식">경양식,일식</option>
+							<option value="기타외식업(다방,패스트푸드등)">
+								기타외식업(다방,패스트푸드등)
+							</option>
+						</select>
+						<S.ShopFilter>
+							{this.state.DATA.map((x) =>
+								x.sh_id === '00000000' ||
+								x.induty_code_se_name !== this.state.selectedFilter ? (
+									''
+								) : (
+									<button
+										type="button"
+										value={this.state.DATA.indexOf(x)}
+										style={{ height: '100px', fontSize: '3vh' }}
+										onClick={(e) =>
+											this.changeShopDetails(e, x.latitude, x.longitude)
+										}
+									>
+										{x.sh_name}
+									</button>
+								)
+							)}
+						</S.ShopFilter>
 					</div>
-					<div>
-						<h2>
-							특이사항
+					<S.ShopDetails>
+						<div>
 							<br />
-							{this.state.DATA[this.state.now].sh_pride.split('\n').map((line) => (
-								<span>
-									▷ {line}
-									<br />
-								</span>
-							))}
-						</h2>
-					</div>{' '}
-					<br />
-					<div>
-						<h3>
-							{this.state.DATA[this.state.now].sh_info.split('\n').map((line) => (
-								<span>
-									{line}
-									<br />
-								</span>
-							))}
-						</h3>
-					</div>
-					<div>
-						<h2>
-							전화번호
+							<h1>{this.state.DATA[this.state.now].sh_name}</h1>
 							<br />
-						</h2>
-						<h3>▶{this.state.DATA[this.state.now].sh_phone}</h3>
+						</div>
+						<div>
+							<h2>
+								특이사항
+								<br />
+								{this.state.DATA[this.state.now].sh_pride
+									.split('\n')
+									.map((line) => (
+										<span>
+											▷ {line}
+											<br />
+										</span>
+									))}
+							</h2>
+						</div>{' '}
 						<br />
-						<h2>
-							좌석 수<br />
-						</h2>
-						<h3>▶{this.state.DATA[this.state.now].seat_num}</h3>
-						<br />
-						<h2>
-							찾아오시는 길<br />
-						</h2>
-						<h3>▶{this.state.DATA[this.state.now].sh_way}</h3>
-						<br />
-					</div>
-				</S.ShopDetails>
+						<div>
+							<h3>
+								{this.state.DATA[this.state.now].sh_info.split('\n').map((line) => (
+									<span>
+										{line}
+										<br />
+									</span>
+								))}
+							</h3>
+						</div>
+						<div>
+							<h2>
+								전화번호
+								<br />
+							</h2>
+							<h3>▶{this.state.DATA[this.state.now].sh_phone}</h3>
+							<br />
+							<h2>
+								좌석 수<br />
+							</h2>
+							<h3>▶{this.state.DATA[this.state.now].seat_num}</h3>
+							<br />
+							<h2>
+								찾아오시는 길<br />
+							</h2>
+							<h3>▶{this.state.DATA[this.state.now].sh_way}</h3>
+							<br />
+						</div>
+					</S.ShopDetails>
+				</S.ContainerPC>
 				<S.ShopS id="map"> </S.ShopS>
 			</S.ShopContainer>
 		);
--- client/src/components/shop/style.js
+++ client/src/components/shop/style.js
@@ -5,25 +5,53 @@ export const ShopContainer = styled.div`
 	display: flex;
 	height: 100%;
 	flex-direction: row;
+	overflowy: auto;
+	@media screen and (max-width: 500px) {
+		flex-direction: column;
+	}
 `;
 
 export const ShopFilter = styled.div`
-    display: flex;
-    flex: 1;
-    background-color: #bfbab078;
+	display: flex;
+	flex: 1;
+	background-color: #cce3de;
 	flex-direction: column;
 	height: auto;
-	overflowY: auto;
+	overflowy: auto;
+	@media screen and (max-width: 500px) {
+		flex-direction: column;
+		height: 20vh;
+	}
 `;
 
 export const ShopDetails = styled.div`
-    display: flex;
-    flex: 1 1 10%;
+	display: flex;
+	flex: 1 1 10%;
 	flex-direction: column;
-    background-color: #f8ede8;
+	background-color: #eaf4f4;
+	width: 20vw;
+	overflow: auto;
+	@media screen and (max-width: 500px) {
+		width: 30vw;
+		height: 60vh;
+	}
 `;
 
 export const ShopS = styled.div`
 	flex: 1 1 40%;
 	padding-bottom: 100px;
+	@media screen and (max-width: 500px) {
+		width: 100vw;
+		height: 100vw;
+	}
+`;
+
+export const ContainerPC = styled.div`
+	display: flex;
+	height: 100%;
+	flex-direction: row;
+	overflowy: auto;
+	@media screen and (max-width: 500px) {
+		flex-direction: row;
+	}
 `;
