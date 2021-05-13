b9146b5e1383693b68c38c1bd56301591f0fda08
jay <dkel03@korea.ac.kr>
2021-05-13 05:40:25 +0000

    feat: backend 기본 세팅
    Signed-off-by: jay <dkel03@korea.ac.kr>

--- server/app.js
+++ server/app.js
@@ -1,20 +1,26 @@
-require('dotenv').config();
+// Import Modules
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// Router Part
+require('dotenv').config();
 const port = process.env.PORT;
 const apiRouter = require('./routes/index');
-
 app.use('/api', apiRouter);
 
+// React based Web provide
 app.use(express.static(path.join(__dirname, '../client', 'build')));
 app.use(express.static('public'));
-
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 });
 
-// start express server on port 5000
+// Cors Setting
+app.use(cors());
+
+// start express server on port
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
