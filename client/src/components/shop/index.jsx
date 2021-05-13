16ed2f31f0f040e5f95cee419fd2024cc14210d0
dooking <123456ghghgh@naver.com>
2021-05-13 23:20:05 +0900

    feat: compoent 구성

---
+++ client/src/components/shop/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './style'
+
+function Price() {
+    return <S.PriceContainer>Price</S.PriceContainer>;
+}
+
+export default Price;
---
+++ client/src/components/sidebar/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { AiTwotoneHome } from "react-icons/ai";
+import { BsBucket, BsBarChart, BsPersonFill } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import * as S from './style'
+
+function Sidebar(props) {
+    const { toggle, toggleHandler } = props
+    return (
+        <S.Container>
+            <S.OutSide onClick={toggleHandler} />
+            <S.SidebarContainer>
+                SideBar
+                <Link to="/">
+                    <S.ItemBox onClick={toggleHandler}>
+                        <AiTwotoneHome />
+                        <S.ItemMenu>홈</S.ItemMenu>
+                    </S.ItemBox>
+                </Link>
+
+                <S.Divider />
+
+                <Link to="/intro">
+                    <S.ItemBox onClick={toggleHandler}>
+                        <BsBucket />
+                        <S.ItemMenu>착한 소비란?</S.ItemMenu>
+                    </S.ItemBox>
+                </Link>
+
+                <S.Divider />
+
+                <Link to="/shop">
+                    <S.ItemBox onClick={toggleHandler}>
+                        <BsBucket />
+                        <S.ItemMenu>착한 업소</S.ItemMenu>
+                    </S.ItemBox>
+                </Link>
+
+
+                <Link to="/price">
+                    <S.ItemBox onClick={toggleHandler}>
+                        <BsBarChart />
+                        <S.ItemMenu>착한 물가</S.ItemMenu>
+                    </S.ItemBox>
+                </Link>
+
+                <S.Divider />
+                <S.ItemBox>
+                    <BsPersonFill />
+                    <S.ItemMenu>About</S.ItemMenu>
+                </S.ItemBox>
+            </S.SidebarContainer>
+        </S.Container >
+
+    );
+}
+
+export default Sidebar;
